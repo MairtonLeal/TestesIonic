03d6e8b8cf2de3e9b85e0c50db7198ad6464631b
Jony Schulz <jony.schulz@salveanoiva.com.br>
2021-02-19 23:35:21 -0300

    teste para o github

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
+import { Screenshot } from '@ionic-native/screenshot/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-
+import { DatePicker } from '@ionic-native/date-picker/ngx';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [DatePicker, Screenshot, { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,83 @@
 import { Component } from '@angular/core';
-
+import { DatePicker } from '@ionic-native/date-picker/ngx';
+import { Screenshot } from '@ionic-native/screenshot/ngx';
+import { AlertController } from '@ionic/angular';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  screen: any;
+  state: boolean = false;
+  constructor(private datePicker: DatePicker, private screenshot: Screenshot, public alertController: AlertController) { }
 
-  constructor() {}
+  mostrarDate() {
+    this.datePicker.show({
+      date: new Date(),
+      mode: 'datetime',
+      androidTheme: this.datePicker.ANDROID_THEMES.THEME_HOLO_DARK
+    }).then(
+      date => alert(date),
+
+      err => console.log('Error occurred while getting date: ', err)
+    );
+  }
+
+  reset() {
+    var self = this;
+    setTimeout(function () {
+      self.state = false;
+    }, 1000);
+  }
+
+  screenShot() {
+    this.screenshot.save('jpg', 80, 'myscreenshot.jpg').then(res => {
+      this.screen = res.filePath;
+      this.state = true;
+      this.reset();
+    });
+  }
+  screenShotURI() {
+    this.screenshot.URI(80).then(res => {
+      this.screen = res.URI;
+      this.state = true;
+      this.reset();
+    });
+  }
+
+
+  async addCustomTask() {
+    const alert = await this.alertController.create({
+      header: 'Add custom task',
+      mode: 'md',
+      inputs: [
+
+        {
+          name: 'data1',
+          placeholder: 'Data, e Hora',
+          type: 'datetime-local',
+          label: 'clique para data',
+
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            return false;
+          }
+        }, {
+          text: 'Ok',
+          handler: (alertData) => {
+            console.log(alertData.data1);
+          }
+        }
+      ]
+    });
+    await alert.present();
+  };
 
 }
